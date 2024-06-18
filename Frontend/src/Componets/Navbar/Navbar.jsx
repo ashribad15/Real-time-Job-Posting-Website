4c4c10024d96ec1a61836e5f4022d2476dc764bf
ASHRIBAD PANDA <133385084+ashribad15@users.noreply.github.com>
2024-06-18 20:54:50 +0530

    Update Navbar.jsx

--- Frontend/src/Componets/Navbar/Navbar.jsx
+++ Frontend/src/Componets/Navbar/Navbar.jsx
@@ -80,13 +80,13 @@ function Navbar() {
                     </div>
                     <div className="elem">
                         <Link to={"/Internship"}>
-                            <p id='int' className='' onMouseEnter={showInternships}>
-                                Internships <i id='ico' className="bi bi-caret-down-fill" onClick={hideInternships}></i>
+                            <p id='int' className='' onMouseEnter={showInternships} onMouseLeave={hideInternships}>
+                                Internships <i id='ico' className="bi bi-caret-down-fill"></i>
                             </p>
                         </Link>
                         <Link to={"/Jobs"}>
-                            <p onMouseEnter={showJobs}>
-                                Jobs <i className="bi bi-caret-down-fill" id='ico2' onClick={hideJobs}></i>
+                            <p onMouseEnter={showJobs} onMouseLeave={hideJobs}>
+                                Jobs <i className="bi bi-caret-down-fill" id='ico2'></i>
                             </p>
                         </Link>
                     </div>
@@ -98,8 +98,8 @@ function Navbar() {
                         <>
                             <div className='Profile'>
                                 <Link to={"/profile"}>
-                                    <img src={user?.photo} alt="profile" onMouseEnter={showProfile} className='rounded-full w-12' id='picpro' />
-                                    <i className='bi bi-caret-up-fill' id='ico3' onClick={hideProfile}></i>
+                                    <img src={user?.photo} alt="profile" onMouseEnter={showProfile} onMouseLeave={hideProfile} className='rounded-full w-12' id='picpro' />
+                                    <i className='bi bi-caret-up-fill' id='ico3'></i>
                                 </Link>
                             </div>
                         </>
@@ -128,7 +128,7 @@ function Navbar() {
                 </ul>
             </nav>
             {isDivVisibleForIntern && (
-                <div className="profile-dropdown-2">
+                <div className="profile-dropdown-2" onMouseEnter={showInternships} onMouseLeave={hideInternships}>
                     <div className="left-section">
                         <p>Top Locations</p>
                         <p>Profile</p>
@@ -146,7 +146,7 @@ function Navbar() {
                 </div>
             )}
             {isDivVisibleForJob && (
-                <div className="profile-dropdown-1">
+                <div className="profile-dropdown-1" onMouseEnter={showJobs} onMouseLeave={hideJobs}>
                     <div className="left-section">
                         <p>Top Locations</p>
                         <p>Profile</p>
@@ -182,7 +182,7 @@ function Navbar() {
                                     <p onClick={loginFunction} className='flex items-center h-9 justify-center mt-4 text-white bg-slate-100 rounded-lg hover:bg-gray-100'>
                                         <div className="px-4 py-3">
                                             <svg className="h-6 w-6" viewBox="0 0 40 40">
-                                                <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
+                                                <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
                                                 <path d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z" fill="#FF3D00" />
                                                 <path d="M20 36.6667C24.305 36.6667 28.2242 35.0175 31.1792 32.3417L26.0483 27.9267C24.3125 29.3583 22.195 30.1667 20 30.1667C15.6683 30.1667 11.9875 27.395 10.6025 23.5375L5.16663 27.7433C7.91579 32.9517 13.505 36.6667 20 36.6667Z" fill="#4CAF50" />
                                                 <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7725 25.1367 27.6333 26.7083 26.0475 27.9267L26.0483 27.9263L31.1792 32.3413C30.8158 32.6733 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#1976D2" />
@@ -203,7 +203,7 @@ function Navbar() {
                                     <p onClick={loginFunction} className='flex items-center h-9 justify-center mt-4 text-white bg-slate-100 rounded-lg hover:bg-gray-100'>
                                         <div className="px-4 py-3">
                                             <svg className="h-6 w-6" viewBox="0 0 40 40">
-                                                <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
+                                                <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
                                                 <path d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z" fill="#FF3D00" />
                                                 <path d="M20 36.6667C24.305 36.6667 28.2242 35.0175 31.1792 32.3417L26.0483 27.9267C24.3125 29.3583 22.195 30.1667 20 30.1667C15.6683 30.1667 11.9875 27.395 10.6025 23.5375L5.16663 27.7433C7.91579 32.9517 13.505 36.6667 20 36.6667Z" fill="#4CAF50" />
                                                 <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7725 25.1367 27.6333 26.7083 26.0475 27.9267L26.0483 27.9263L31.1792 32.3413C30.8158 32.6733 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#1976D2" />
@@ -221,7 +221,7 @@ function Navbar() {
                 </div>
             )}
             {isDivVisibleForProfile && (
-                <div className="profile-dropdown-3">
+                <div className="profile-dropdown-3" onMouseEnter={showProfile} onMouseLeave={hideProfile}>
                     <div className="left-section">
                         <p>Profile</p>
                         <p>Profile</p>
